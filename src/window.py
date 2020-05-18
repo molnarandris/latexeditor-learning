3b266ff95191faf228238601a2df230c96c9b868
Andras Molnar <molnarandris@gmail.com>
2020-05-18 17:48:32 +0200

    Refactoring the spawning process

--- src/window.py
+++ src/window.py
@@ -22,9 +22,9 @@ from gi.repository import Gtk
 from gi.repository import Gdk
 from gi.repository import Gio
 from gi.repository import GObject
-from gi.repository import GLib
 from .documentmanager import Documentmanager
 from .pdfviewer import PdfViewer
+from .processrunner import GAsyncSpawn
 
 gi.require_version('GtkSource', '3.0')
 from gi.repository import GtkSource
@@ -99,54 +99,6 @@ class WindowStateSaver:
         return False
 
 
-class GAsyncSpawn(GObject.GObject):
-    """ GObject class to wrap GLib.spawn_async().
-
-    Use:
-        s = GAsyncSpawn()
-        s.connect('process-done', mycallback)
-        s.run(command)
-            #command: list of strings
-    """
-    __gsignals__ = {
-        'process-done': (GObject.SIGNAL_RUN_LAST, GObject.TYPE_NONE,
-                         (GObject.TYPE_INT, )),
-        'stdout-data': (GObject.SIGNAL_RUN_LAST, GObject.TYPE_NONE,
-                        (GObject.TYPE_STRING, )),
-        'stderr-data': (GObject.SIGNAL_RUN_LAST, GObject.TYPE_NONE,
-                        (GObject.TYPE_STRING, )),
-    }
-
-    def __init__(self):
-        GObject.GObject.__init__(self)
-
-    def run(self, cmd):
-        r = GLib.spawn_async(cmd,
-                             flags=GLib.SPAWN_DO_NOT_REAP_CHILD,
-                             standard_output=True,
-                             standard_error=True)
-        self.pid, idin, idout, iderr = r
-        fout = os.fdopen(idout, "r")
-        ferr = os.fdopen(iderr, "r")
-
-        GLib.child_watch_add(self.pid, self._on_done)
-        GLib.io_add_watch(fout, GLib.IO_IN, self._on_stdout)
-        GLib.io_add_watch(ferr, GLib.IO_IN, self._on_stderr)
-        return self.pid
-
-    def _on_done(self, pid, retval, *argv):
-        self.emit("process-done", retval)
-
-    def _emit_std(self, name, value):
-        self.emit(name+"-data", value)
-
-    def _on_stdout(self, fobj, cond):
-        self._emit_std("stdout", fobj.readline())
-        return True
-
-    def _on_stderr(self, fobj, cond):
-        self._emit_std("stderr", fobj.readline())
-        return True
 
 @Gtk.Template(filename="src/window.ui")
 class MathwriterWindow(Gtk.ApplicationWindow):
@@ -263,3 +215,5 @@ class MathwriterWindow(Gtk.ApplicationWindow):
             self.pdf_viewer.reload()
         else:
             print("compile finished with error")
+        sender.disconnect_by_func(self.on_compile_finish)
+        sender.my_destroy()
